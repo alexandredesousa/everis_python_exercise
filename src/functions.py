afd521b7055057c5553dc3d6cef179de55d70a24
alexandredesousa_mbp <alexdesousa@sapo.pt>
2021-07-30 22:33:19 +0100

    adds function compute solution that computes the solution table from the raw data and the previously developed function. first as list of lists (per year) then converted to a pandas dataframe.

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,7 @@
+import pandas as pd
+
 # functions as in excel
-def fixed(year_t1_val, fixed_rate):
+def fixed_function(year_t1_val, fixed_rate):
     return year_t1_val*(1+fixed_rate)
 
 def xpi_function(aux_val, year_t, xpi_table):
@@ -19,3 +21,46 @@ def www_function(year_t, www_table):
 def element_percent_increase_function(percentage, preceding_element_t, preceding_element_t1):
     ""
     return preceding_element_t + preceding_element_t1 * percentage
+
+def compute_solution(xpi_data, www_data, aux_val, fixed_rate, perc_one_rate, perc_two_rate):
+
+    head = ['year', 'fixed', 'xpi', 'xpi+1.0%', 'xpi+2.0%', 'www', 'www+1.0%', 'www+2.0%']
+    solution_zero = [2021, 1.00, 1.00, 1.00, 1.00, www_function(2021, www_data),1.00, 1.00]
+
+    computed_solution = [solution_zero]
+
+    year_start = 2020
+    year_end = 1990
+    # year_end = 2018
+
+    for i in range(year_start, year_end-1, -1):
+        current_year = i
+
+        # fixed
+        fixed = fixed_function(computed_solution[-1][1], fixed_rate)
+
+        # XPI
+        xpi_current = xpi_function(aux_val, current_year, xpi_data)
+        xpi_t1 = computed_solution[-1][2]
+        xpi1p_current = element_percent_increase_function(perc_one_rate, xpi_current, xpi_t1)
+        xpi1p_t1 = computed_solution[-1][3]
+        xpi2p_current = element_percent_increase_function(perc_two_rate, xpi1p_current, xpi1p_t1)
+
+        # WWW
+        www_current = www_function(current_year, www_data)
+        www_t1 = computed_solution[-1][5]
+        www1p_current = element_percent_increase_function(perc_one_rate, www_current,www_t1)
+        www1p_t1 = computed_solution[-1][6]
+        www2p_current = element_percent_increase_function(perc_two_rate, www1p_current, www1p_t1)
+
+        computed_solution.append([current_year,
+                                  fixed,
+                                  xpi_current, xpi1p_current, xpi2p_current,
+                                  www_current, www1p_current, www2p_current])
+
+    solution_df = pd.DataFrame(computed_solution, columns=head)
+
+    print("almost there")
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,41 +1,16 @@
 import pandas as pd
 from src import functions
 
+# raw data
 xpi_data = pd.read_csv('../data/XPI_data.csv')
 aux_data = pd.read_csv('../data/AUX_data.csv')
 perc_data = pd.read_csv('../data/PERC_data.csv')
 www_data = pd.read_csv('../data/WWW_data.csv')
 
-# testing Fixed function
-fixed_rate = perc_data[perc_data.Ref == 'fixed']['Percentage'].item()
-test_fixed_2000 = round(functions.fixed(1.81, fixed_rate), 2)
-test_fixed_2009 = round(functions.fixed(1.38, fixed_rate), 2)
-
-# testing XPI function
 aux_val = aux_data[aux_data.Type == '2021']['Value'].item()
-test_XPI_2000 = round(functions.xpi_function(aux_val, 2000, xpi_data), 2)
-test_XPI_2009 = round(functions.xpi_function(aux_val, 2009, xpi_data), 2)
 
-# testing WWW function
-test_WWW_2000 = round(functions.www_function(2000, www_data), 2)
-test_WWW_2009 = round(functions.www_function(2009, www_data), 2)
-
-# testing percentages functions
+fixed_rate = perc_data[perc_data.Ref == 'fixed']['Percentage'].item()
 perc_one = perc_data[perc_data.Ref == 'perc_one']['Percentage'].item()
 perc_two = perc_data[perc_data.Ref == 'perc_two']['Percentage'].item()
 
-# testing XPI+1.0%
-test_XPI1_2000 = round(functions.element_percent_increase_function(perc_one, test_XPI_2000, 1.48), 2)
-test_XPI1_2009 = round(functions.element_percent_increase_function(perc_one, test_XPI_2009, 1.20), 2)
-
-# testing XPI+2.0%
-test_XPI2_2000 = round(functions.element_percent_increase_function(perc_two, test_XPI1_2000, 1.49), 2)
-test_XPI2_2009 = round(functions.element_percent_increase_function(perc_two, test_XPI1_2009, 1.21), 2)
-
-# testing WWW+1.0%
-test_WWW1_2000 = round(functions.element_percent_increase_function(perc_one, test_WWW_2000, 1.64), 2)
-test_WWW1_2009 = round(functions.element_percent_increase_function(perc_one, test_WWW_2009, 1.30), 2)
-
-# testing WWW+2.0%
-test_WWW2_2000 = round(functions.element_percent_increase_function(perc_two, test_WWW1_2000, 1.49), 2)
-test_WWW2_2009 = round(functions.element_percent_increase_function(perc_two, test_WWW1_2009, 1.21), 2)
+functions.compute_solution(xpi_data, www_data, aux_val, fixed_rate, perc_one, perc_two)
